f5e2cb009b7105607d0e79c9bb906d482eac8d81
DominicusPlatus <dominik.guz11@gmail.com>
2016-05-29 21:41:27 +0200

    initial import, views, model for data, services

---
+++ sconnJTAG/sconnJTAG/View/Scan/BoundaryScanDefinitionsListView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace sconnJTAG.View
+{
+	public class BoundaryScanDefinitionsListView : ContentPage
+	{
+		public BoundaryScanDefinitionsListView ()
+		{
+			Content = new StackLayout {
+				Children = {
+					new Label { Text = "Hello ContentPage" }
+				}
+			};
+		}
+	}
+}
