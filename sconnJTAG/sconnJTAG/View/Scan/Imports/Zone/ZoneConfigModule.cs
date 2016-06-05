1575bc87d854fc3599858afe70f7070781f7cbbc
Dominik Guz <dominik.guz11@gmail.com>
2016-06-05 13:12:01 +0200

    models, data

---
+++ sconnJTAG/sconnJTAG/View/Scan/Imports/Zone/ZoneConfigModule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mef.Modularity;
+using Prism.Modularity;
+using Prism.Regions;
+using sconnRem.Navigation;
+using sconnRem.View.Config.AlarmSystem.Auth;
+
+namespace sconnRem.View.Config.AlarmSystem.Zone
+{
+    [ModuleExport(typeof(ZoneConfigModule))]
+    public class ZoneConfigModule : IModule
+    {
+        [Import]
+        public IRegionManager RegionManager;
+
+        public void Initialize()
+        {
+            this.RegionManager.RegisterViewWithRegion(AlarmRegionNames.MainNavigationRegion, typeof(ZoneConfigViewNavigationItem));
+        }
+    }
+
+}
